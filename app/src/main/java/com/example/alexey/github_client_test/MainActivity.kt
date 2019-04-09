07fd6aa279947b4347f2845b3cd9005fce67776c
Alexey <bauer080@gmail.ru>
2019-04-09 22:17:09 +0500

    add activity_main.xml

--- app/src/main/java/com/example/alexey/github_client_test/MainActivity.kt
+++ app/src/main/java/com/example/alexey/github_client_test/MainActivity.kt
@@ -1,6 +1,21 @@
-package com.example.alexey.limitless
+package com.example.alexey.github_client_test
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity()
+class MainActivity: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val articleTitle = MutableList(4, {x -> "Item$x"})
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, articleTitle)
+        listView.adapter = adapter
+    }
+}
